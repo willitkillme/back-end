6fb654ec5fcf268f2f1763c05d0b234c3aecdddd
Saltybusdriver <K.paco2000@gmail.com>
2024-05-28 03:31:41 +0200

    function now takes 2 arguments

--- wikm/wikmapp/prodFunctions.py
+++ wikm/wikmapp/prodFunctions.py
@@ -10,42 +10,39 @@ ingredients = {
         "Farro", "Bulgur", "Semolina", "Couscous", "Durum wheat", "Graham flour", "Malt", 
         "Malt extract", "Malt syrup", "Brewer's yeast", "Wheat starch", "Modified wheat starch", 
         "Hydrolyzed wheat protein", "Seitan (wheat gluten)", "Wheat bran", "Wheat germ", "Einkorn", 
-        "Emmer", "Matzo (unless specifically gluten-free)", "Udon noodles", "Pasta (unless gluten-free)", 
-        "Bread (unless gluten-free)", "Crackers (unless gluten-free)", "Cookies (unless gluten-free)", 
-        "Cakes (unless gluten-free)", "Pastries (unless gluten-free)", "Pizza crust (unless gluten-free)", 
-        "Beer (unless gluten-free)", "Some soy sauces (unless labeled gluten-free)", "Some processed foods (check labels for hidden gluten)"
+        "Emmer", "Matzo ", "Udon noodles", "Pasta", 
+        "Bread ", "Crackers", "Cookies", 
+        "Cakes", "Pastries", "Pizza crust", 
+        "Beer", "Some soy sauces", "Some processed foods"
     ],
     "eggs": [
-        "Eggs (whole, whites, yolks)", "Egg powder", "Eggnog", "Mayonnaise", "Aioli", "Meringue", 
-        "Custard", "Pudding (some types)", "Quiche", "Frittata", "Omelet", "Scrambled eggs", 
-        "Deviled eggs", "Egg noodles", "Pasta (some types)", "Egg wash (used in baking)", 
+        "Eggs", "Egg powder", "Eggnog", "Mayonnaise", "Aioli", "Meringue", 
+        "Custard", "Pudding", "Quiche", "Frittata", "Omelet", "Scrambled eggs", 
+        "Deviled eggs", "Egg noodles", "Pasta", "Egg wash", 
         "Baked goods (many types of bread, cakes, muffins, cookies, pastries)", "French toast", 
-        "Pancakes", "Waffles", "Some salad dressings (e.g., Caesar dressing)", "Some sauces (e.g., hollandaise, béarnaise)", 
-        "Meatloaf (often contains egg as a binder)", "Meatballs (often contains egg as a binder)", 
-        "Breaded or battered foods (the coating often contains egg)", "Ice cream (some types)", 
-        "Marshmallows (some types)", "Marzipan (some types)", "Some candies (e.g., certain chocolates, nougat)", 
-        "Certain processed foods (always check labels)"
+        "Pancakes", "Waffles", 
+        "Meatloaf", "Meatballs", 
+        "Breaded or battered food", "Ice cream", 
+        "Marshmallows", "Marzipan", "Some candies"
     ],
     "soy": [
         "Soybeans", "Soy sauce", "Tamari", "Miso", "Tempeh", "Tofu", "Edamame", "Soy milk", 
         "Soy yogurt", "Soy cheese", "Soy protein isolate", "Soy protein concentrate", "Soy lecithin", 
         "Textured vegetable protein (TVP)", "Natto", "Soy flour", "Soybean oil", 
-        "Soy nuts", "Soy nut butter", "Shoyu", "Teriyaki sauce (often contains soy sauce)", 
-        "Vegetable broth (sometimes contains soy)", "Hydrolyzed soy protein", "Soy crisps", 
-        "Meat substitutes (many vegetarian and vegan products like veggie burgers, sausages, and deli slices)", 
-        "Some baked goods (check labels)", "Some cereals (check labels)", "Some snack foods (check labels)", 
-        "Some chocolate and candy (check labels)", "Some infant formulas (check labels)"
+        "Soy nuts", "Soy nut butter", "Shoyu", "Teriyaki sauce", 
+        "Vegetable broth", "Hydrolyzed soy protein", "Soy crisps", 
+        "Meat substitutes"
     ],
     "fish_and_seafood": [
-        "Fish (all types)", "Salmon", "Tuna", "Cod", "Haddock", "Halibut", "Mackerel", "Sardines", 
-        "Anchovies", "Tilapia", "Trout", "Bass", "Shellfish (all types)", "Shrimp", "Crab", "Lobster", 
+        "Fish", "Salmon", "Tuna", "Cod", "Haddock", "Halibut", "Mackerel", "Sardines", 
+        "Anchovies", "Tilapia", "Trout", "Bass", "Shellfish", "Shrimp", "Crab", "Lobster", 
         "Clams", "Mussels", "Oysters", "Scallops", "Squid (calamari)", "Fish sauce", "Oyster sauce", 
-        "Worcestershire sauce (some brands contain anchovies)", "Caviar", "Roe", "Imitation crab (surimi)", 
-        "Fish stock", "Fish broth", "Bouillabaisse (fish stew)", "Pâté (some types)", 
-        "Seafood soup (clam chowder, lobster bisque, etc.)", "Sushi and sashimi", "Seafood salad", 
-        "Seafood-based dips", "Fish sticks/fish fingers", "Breaded or battered seafood", 
-        "Canned fish (tuna, salmon, sardines, etc.)", "Dried fish", "Pickled fish (herring, etc.)", 
-        "Smoked fish (salmon, mackerel, etc.)"
+        "Worcestershire sauce", "Caviar", "Roe", "Imitation crab (surimi)", 
+        "Fish stock", "Fish broth", "Bouillabaisse", "Pâté", 
+        "Seafood soup", "Sushi and sashimi", "Seafood salad", 
+        "Seafood-based dips", "Fish sticks","fish fingers", "Breaded seafood","battered seafood", 
+        "Canned fish", "Dried fish", "Pickled fish", 
+        "Smoked fish"
     ]
 }
 
@@ -70,3 +67,70 @@ def GetProdData(barcode):
 
 
 #NOTE: IDE ÚJ FUNCTION
+
+data_json = None
+
+def _process_ingredient_list(data): #pass extracted info from barcode.
+    ingredients_str = data['data'][0]['ingredients_translations']['en']
+    ingredients_list = [ingredient.strip() for ingredient in ingredients_str.split(',')]
+    for i in range(len(ingredients_list)):
+        ingredients_list[i] = ingredients_list[i].replace('.', '')
+    return ingredients_list
+
+def _compute_lps_array(pattern):
+    m = len(pattern)
+    lps = [0] * m
+    length = 0
+    i = 1
+
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    return lps
+
+def _kmp_search(text, pattern):
+    n = len(text)
+    m = len(pattern)
+
+    lps = compute_lps_array(pattern)
+    
+    i = 0 
+    j = 0 
+    
+    while i < n:
+        if pattern[j] == text[i]:
+            i += 1
+            j += 1
+        
+        if j == m:
+            #print("GAY")
+            return True
+        
+        elif i < n and pattern[j] != text[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return False
+
+def check_for_allergens(data_json,user_allergy):
+    allergictto=[]
+    data = json.loads(data_json)
+    ing_list=process_ingredient_list(data)
+    
+    for ingredient in ing_list:
+        for allergen, items in ingredients.items():
+            for item in items:
+                #if ingredient.lower() == item.lower():
+                if _kmp_search(item.lower(),ingredient.lower()): ##comment out if dont work, untested shi
+                    allergictto.append(allergen)
+
+    return [item for item in allergictto if item in user_allergy]
